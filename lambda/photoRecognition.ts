8c16317a75e4f06b30fe0cadd40807575241cf9b
SOMESH BANERJEE <54861461+somesh-banerjee@users.noreply.github.com>
2025-06-24 22:15:52 +0530

    Add placeholders for Lambda  and Step Functions for photo processing workflow

---
+++ lambda/photoRecognition.ts
@@ -0,0 +1,12 @@
+// imports
+
+
+// helper methods
+
+
+
+// lambda entry point
+export const handler = async (event: any, context: any) => {
+    // Your lambda function logic here
+  };
+  
--- lib/photo-recognition-stack.ts
+++ lib/photo-recognition-stack.ts
@@ -1,16 +1,66 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as sfn from 'aws-cdk-lib/aws-stepfunctions';
+import * as tasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
 
 export class PhotoRecognitionStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'PhotoRecognitionQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const photoUploadFunction = new lambda.Function(this, 'PhotoUploadFunction', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'photoUpload.handler',
+      environment: {
+        BUCKET_NAME: 'my-photo-bucket',
+      },
+    });
+
+    const photoRecognitionFunction = new lambda.Function(this, 'PhotoRecognitionFunction', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'photoRecognition.handler',
+      environment: {
+        RECOGNITION_MODEL: 'my-recognition-model',
+      },
+    });
+
+    const photoUploadTask = new tasks.LambdaInvoke(this, 'PhotoUploadTask', {
+      lambdaFunction: photoUploadFunction,
+      outputPath: '$.Payload',
+    });
+
+    const photoRecognitionTask = new tasks.LambdaInvoke(this, 'PhotoRecognitionTask', {
+      lambdaFunction: photoRecognitionFunction,
+      outputPath: '$.Payload',
+    });
+
+    const definition = photoUploadTask
+      .next(photoRecognitionTask)
+      .next(new sfn.Succeed(this, 'Success'));
+
+    const stateMachine = new sfn.StateMachine(this, 'PhotoRecognitionStateMachine', {
+      definition,
+      timeout: cdk.Duration.minutes(5),
+    });
+
+    new cdk.CfnOutput(this, 'StateMachineArn', {
+      value: stateMachine.stateMachineArn,
+      description: 'The ARN of the photo recognition state machine',
+    });
+    new cdk.CfnOutput(this, 'PhotoUploadFunctionArn', {
+      value: photoUploadFunction.functionArn,
+      description: 'The ARN of the photo upload Lambda function',
+    });
+    new cdk.CfnOutput(this, 'PhotoRecognitionFunctionArn', {
+      value: photoRecognitionFunction.functionArn,
+      description: 'The ARN of the photo recognition Lambda function',
+    });
+    new cdk.CfnOutput(this, 'BucketName', {
+      value: 'my-photo-bucket',
+      description: 'The name of the S3 bucket for photo uploads',
+    });
   }
 }
